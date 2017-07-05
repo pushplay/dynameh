3622f0909180615f1f377bfae16fefe8d9a565b3
Filip Danić <filip@spicefactory.co>
2017-07-05 22:56:38 +0200

    Revert "temporally include distribution for hot fix"

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as batchHelper from "./batchHelper";
-import * as concurrentHelper from "./concurrentHelper";
-import * as requestBuilder from "./requestBuilder";
-import * as requestUnwrapper from "./requestUnwrapper";
-import * as responseUnwrapper from "./responseUnwrapper";
-import { TableSchema } from "./TableSchema";
-export { batchHelper, concurrentHelper, requestBuilder, requestUnwrapper, responseUnwrapper, TableSchema };
--- dist/requestUnwrapper.d.ts
+++
@@ -1,5 +0,0 @@
-import * as aws from "aws-sdk";
-/**
- * Extract the item being put from the input.
- */
-export declare function unwrapPutInput(input: aws.DynamoDB.Types.PutItemInput): any;
